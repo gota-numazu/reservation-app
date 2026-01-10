2d498803d92cdfe7973a698ee74e2753d17f5b89
gota-numazu <gouta611@gmail.com>
2026-01-10 12:01:47 +0900

    Angularフロントエンド側作成

---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../../products';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrl: './product-listings.component.scss',
+})
+export class ProductListComponent implements OnInit {
+  products: any;
+  constructor() {}
+
+  ngOnInit() {
+    this.products = products;
+  }
+}
